571906aa62e95425f1c6bc77a5cf161d05fbb27c
Lucas Joaquin Vanini <ljvanini92@gmail.com>
2022-05-21 15:02:49 -0300

    Nombre de tienda

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>UNI REGALOS.</h1>
+    <h2>Tazas y remereas sublimadas.</h2>
+  );
+}
+
+export default App;
